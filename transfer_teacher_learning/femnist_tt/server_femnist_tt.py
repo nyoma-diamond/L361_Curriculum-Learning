933fdaab399f185664b319971b0243bfd92b6bdd
Grace Kim <gracekim@MacBook-Pro-250.local>
2024-03-19 01:09:51 -0400

    transfer_teacher_learning

---
+++ transfer_teacher_learning/femnist_tt/server_femnist_tt.py
@@ -0,0 +1,105 @@
+import flwr as fl
+import ray
+import gc
+from client_femnist_tt import client_fn
+from typing import Dict, List, Optional, Tuple, Union
+from flwr.server.client_proxy import ClientProxy
+from flwr.common import (
+    EvaluateRes,
+    FitRes,
+    Scalar,
+)
+import numpy as np
+import pandas as pd
+
+
+TEST = "fem_sp_imgs_per_e"
+ROUND = 50
+EPOCHS = 25
+NUM_CLIENTS = 8
+THRESHOLD_TYPE = 1
+PERCENTILE_TYPE = "linear"
+LOSS_THRESHOLD = 4
+test_lambda = [99.5]
+
+
+
+
+class AggregateCustomMetricStrategy(fl.server.strategy.FedAvg):
+    def aggregate_evaluate(
+        self,
+        server_round: int,
+        results: List[Tuple[ClientProxy, EvaluateRes]],
+        failures: List[Union[Tuple[ClientProxy, FitRes], BaseException]],
+        ) -> Tuple[Optional[float], Dict[str, Scalar]]:
+        """Aggregate evaluation accuracy using weighted average."""
+
+        if not results:
+            return None, {}
+
+        # Call aggregate_evaluate from base class (FedAvg) to aggregate loss and metrics
+        aggregated_loss, aggregated_metrics = super().aggregate_evaluate(server_round, results, failures)
+
+        # Weigh accuracy of each client by number of examples used
+        accuracies = [r.metrics["accuracy"] * r.num_examples for _, r in results]
+        examples = [r.num_examples for _, r in results]
+
+        # Aggregate and print custom metric
+        aggregated_accuracy = sum(accuracies) / sum(examples)
+        print(f"Round {server_round} accuracy aggregated from client results: {aggregated_accuracy}")
+
+        # Return aggregated loss and metrics (i.e., aggregated accuracy)
+        return aggregated_loss, {"accuracy": aggregated_accuracy}
+
+# SelfPaced = fl.server.strategy.FedAvg(
+#     on_fit_config_fn=fit_config        #fit_config,  # For future config function based changes
+# )
+
+
+
+results = {}
+
+
+for _lambda in test_lambda:
+    print('lambda =', _lambda)
+    t_name = TEST +"_r"+ str(ROUND)+ "_e" + str(EPOCHS) + "_c" + str(NUM_CLIENTS) + "_l" +str(THRESHOLD_TYPE) + "_pt-" + str(PERCENTILE_TYPE) + "_lt" + str(_lambda)
+    
+    results = {}
+    # TODO: work on this fit_config function for more specialized cases
+    def fit_config(server_round: int):
+        """Return training configuration dict for each round.
+
+        Perform two rounds of training with one local epoch, increase to two local
+        epochs afterwards.
+        """
+        config = {
+            "server_round": server_round,           # The current round of federated learning
+            "local_epochs": EPOCHS,                 # total epochs
+            "loss_threshold": _lambda,       # depending on what you enter as loss type, this can be actual loss value
+                                                    # or the percentile value you want to test for your scenario
+            "Total_Rounds": ROUND,                  # total # of rounds
+            "test_name": t_name,                 # put a meaningful test name
+            "threshold_type": THRESHOLD_TYPE,       # change 0 for just flat num, 1, for percentile
+            "percentile_type": PERCENTILE_TYPE      # change "linear" for true percentile, "normal_unbiased" for normal, put whatever for flat_num
+        }
+        return config
+
+    # Create strategy and run server
+    SelfPaced = AggregateCustomMetricStrategy(
+        on_fit_config_fn=fit_config,        #fit_config,  # For future config function based changes
+    )
+
+    results[_lambda] = fl.simulation.start_simulation(
+        num_clients=NUM_CLIENTS,
+        clients_ids =["1","2","3","4","5","6","7","8"],
+        client_fn=client_fn,
+        config=fl.server.ServerConfig(num_rounds=ROUND),
+        strategy=SelfPaced
+    )
+    losses_distributed = pd.DataFrame.from_dict({_lambda: [acc for _, acc in results[_lambda].losses_distributed] for _lambda in results.keys()})
+    accuracies_distributed = pd.DataFrame.from_dict({_lambda: [acc for _, acc in results[_lambda].metrics_distributed['accuracy']] for _lambda in results.keys()})
+    losses_distributed.to_csv('results/'+t_name+"/losses_distributed.csv")
+    accuracies_distributed.to_csv('results/'+t_name+"/accuracies_distributed.csv")
+    ray.shutdown()
+    gc.collect()
+
---
+++ transfer_teacher_learning/femnist_tt/utils.py
@@ -0,0 +1,188 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import numpy as np
+from PIL import Image
+import torchvision
+# from torchvision.utils import save_image
+
+#TODO:
+from cv2 import imwrite
+
+def calculate_threshold(net, loss_func, trainloader, loss_threshold, DEVICE, loss_type, percentile_type):
+    loss_indv = []
+
+    if loss_type == 1:
+        for images, labels in trainloader:
+
+            images = images.to(DEVICE)      # @ N'yoma make sure to set images/labels to the device you're using
+            labels = labels.to(DEVICE)
+
+            losses = loss_func(net(images), labels).data.cpu().numpy()
+            for loss in losses:
+                loss_indv.append(loss) # get individual losses
+        
+        return np.percentile(np.array(loss_indv), loss_threshold, method = percentile_type)
+
+    else:
+        return loss_threshold
+
+
+
+def curriculum_learning_loss(net, loss_func, images, labels, loss_threshold, DEVICE):
+    """
+    @ N'yoma i just realized I implemented this for batch per batch rather than epochs... damn
+
+    Inputs: 
+    - net: neural network
+    - loss_func: torch.nn.CrossEntropyLoss(reduction='none')
+        - Make sure to set reduction to none!
+    - Images: batch of images
+    - labels: batch of labels
+    - loss_threshold: depending on what you enter as loss type, this can be actual loss value
+        or the percentile value you want to test for your scenario
+    - DEVICE: CPU/GPU
+    - loss_type: This has the following options:
+        - '0': use loss_threshold value as a singular number if a loss value is too high or low
+        - '1': use loss_threshold value as a percentile in a normal distribution curve to see 
+               if a loss value is too high or low 
+    - percentile_type: check out this link for more info: https://numpy.org/doc/stable/reference/generated/numpy.percentile.html
+        - "Linear": true percentile
+        - "normal_unbiased": sampling from a normal distribution curve
+    """
+
+
+    images = images.to(DEVICE)      # @ N'yoma make sure to set images/labels to the device you're using
+    labels = labels.to(DEVICE)
+
+
+    loss_indv = loss_func(net(images), labels) # get individual losses
+
+    # print(loss_indv)
+    b = loss_indv >= loss_threshold # get indicies of which are larger than loss_threshold
+    trash_indices = b.nonzero()
+    d = loss_indv < loss_threshold # get indicies of which are larger than loss_threshold
+    keep_indices = d.nonzero()
+
+    return trash_indices, keep_indices, loss_threshold, loss_indv
+
+#config['test_name']
+def save_data(losses, images_passed, images_failed, test_name, cid, DEVICE):
+    folder_name = 'results/'+test_name+"/cid_"+str(cid)
+    round = 0
+    x = pd.DataFrame(losses, columns=["sample_loss",
+                                      "loss_threshold_of_batch",
+                                      "epoch",
+                                      "batch_count",
+                                      "labels"])
+
+    if not os.path.exists(folder_name+"/round_0"):
+        os.makedirs(folder_name+"/round_0")
+        os.makedirs(folder_name+"/round_"+str(round)+"/imgs_failed")
+        os.makedirs(folder_name+"/round_"+str(round)+"/imgs_passed")
+
+    else:
+        s = os.listdir(folder_name)
+        s = [int(i.split("_")[-1]) for i in s]
+        round = max(s) + 1
+        print(round)
+        if not os.path.exists(folder_name+"/round_"+str(round)):
+            os.makedirs(folder_name+"/round_"+str(round))
+            os.makedirs(folder_name+"/round_"+str(round)+"/imgs_failed")
+            os.makedirs(folder_name+"/round_"+str(round)+"/imgs_passed")
+    x.to_csv(folder_name+"/round_"+str(round)+"/losses.csv")
+
+    for [image_1,label,loss_ind, loss_threshold,epoch] in images_failed:
+        file_name = folder_name+"/round_"+str(round)+"/imgs_failed/"+str(label.item())+"_"+str(loss_ind.item())+"_"+str(loss_threshold)+"_"+str(epoch)+".jpg"   
+        torchvision.utils.save_image(image_1, file_name)
+    for [image_1,label,loss_ind, loss_threshold,epoch] in images_passed:
+        file_name = folder_name+"/round_"+str(round)+"/imgs_passed/"+str(label.item())+"_"+str(loss_ind.item())+"_"+str(loss_threshold)+"_"+str(epoch)+".jpg"   
+        torchvision.utils.save_image(image_1, file_name)
+    #     # print(image_1)
+    #     # tensor = image_1*255
+    #     # tensor = np.array(tensor.cpu(), dtype=np.uint8)
+    #     # if np.ndim(tensor)>3:
+    #     #     assert tensor.shape[0] == 1
+    #     #     tensor = tensor[0]
+    #     # im = Image.fromarray(tensor)
+    #     # im = im.convert('RGB')
+    #     # im.save(file_name, "PNG")
+    #     # img = Image.fromarray(image_1.cpu())
+    #     # img.save(file_name)
+    #     # batch_tensor=image_1.cpu().data.numpy()
+    #     # batch_tensor=np.array(batch_tensor,dtype=np.uint8)
+        
+    #     # batch_tensor=np.transpose(batch_tensor,(0,2,3,1))
+    #     # for index, image in enumerate(batch_tensor):
+    #     #     ret_tensor=im.fromarray(image)
+    #     #     ret_tensor.save(file_name)
+    #     # print(file_name)
+    #     # image = image_1.cpu().numpy()
+    #     # imwrite(file_name, image)
+    #     # take first image
+    #     # image = image_1.to(DEVICE)
+    #     # # Reshape the image
+    #     # image = image.reshape(3,32,32).to(DEVICE)
+    #     # # Transpose the image
+    #     # image = image.permute(1, 2, 0).to(DEVICE)
+    #     # image = image.numpy()
+    #     # # Display the image
+    #     # file_name = folder_name+"/round_"+str(round)+"/imgs_failed/"+str(label)+"_"+str(loss_ind)+"_"+str(loss_threshold)+'.jpg'    
+    #     # plt.imshow(image.cpu())
+    #     # plt.imsave(file_name, image.cpu())
+
+    #     # count +=1 
+    #     # plt.imshow(image.cpu())
+    #     # plt.show()
+    return
+
+
+# TODO
+def show_failed_imgs(failed_images, failed_labels, losses_failed, DEVICE, test_name, cid):
+    folder_name = 'results/'+test_name+"/cid_"+str(cid)+"failed_img"
+    count = 0
+    s = os.listdir(folder_name)
+    if not os.path.exists(folder_name+"/round_0"):
+        os.makedirs(folder_name+"/round_0")
+    # results folder to save all failed images in
+    if not os.path.exists('results'):
+        os.makedirs('results')
+    if not os.path.exists('results/'+str(batch_count)):
+        os.makedirs('results/'+str(batch_count))
+
+    count = 0
+    # print(len(failed_images))
+        
+    for images in failed_images:
+        # if count > 3:
+        #     break
+
+        # take first image
+        image = images[0].to(DEVICE)
+        # Reshape the image
+        image = image.reshape(3,32,32).to(DEVICE)
+        # Transpose the image
+        image = image.permute(1, 2, 0).to(DEVICE)
+        # Display the image
+        imwrite('results/'+str(count)+'.jpg', image.cpu())
+
+        count +=1 
+        # plt.imshow(image.cpu())
+        # plt.show()
+
+
+"""
+if not os.path.exists('out'):
+    os.makedirs('out')
+if not os.path.exists('results'):
+    os.makedirs('results')
+    
+with open(f'out/{target_name}_{run_uuid}_out.fred', 'w') as file:
+        file.write(filedata.format(**vars))
+        
+
+    imwrite('diff.jpg', diff_image)
+
+    from cv2 import imread, resize, imwrite, erode
+"""
